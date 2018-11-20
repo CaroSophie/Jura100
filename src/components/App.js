ed8a115a4bf7a983fe8648f5dc45ca086343bd59
Caroline Jörgens <caroline.jorgens@aol.com>
2018-11-20 12:04:13 +0100

    WIP: work on router

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,30 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
 import Home from '../Homescreen/Home'
 import Categories from '../Screen2/Categories'
+import uid from 'uid'
 
 export default class App extends Component {
+  zivilrecht = [
+    {
+      id: uid(),
+      theme: 'Arbeitsrecht'
+    },
+
+    {
+      id: uid(),
+      theme: 'Familienrecht'
+    }
+  ]
+
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/" render={() => <Home />} />
+          <Route
+            exact
+            path="/"
+            render={() => <Home zivilrecht={this.zivilrecht} />}
+          />
           <Route path="/categories" render={() => <Categories />} />
           <nav>
             <NavLink exact to="/">
