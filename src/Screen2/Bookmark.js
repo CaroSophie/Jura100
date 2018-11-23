2789c4d762da91750b8f08f78ddbb991a57395e9
Caroline Jörgens <caroline.jorgens@aol.com>
2018-11-23 16:09:00 +0100

    Add bookmark icon

---
+++ src/Screen2/Bookmark.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Icon = styled.div`
+  height: 20px;
+  width: 20px;
+  border: 2px solid black;
+  font-size: 1em;
+  margin-left: auto;
+`
+
+export default class Bookmark extends Component {
+  render() {
+    return <Icon />
+  }
+}
--- src/Screen2/Private.js
+++ src/Screen2/Private.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Bookmark from './Bookmark'
 
 const Wrapper = styled.section`
   height: 100vh;
@@ -8,12 +9,13 @@ const Wrapper = styled.section`
   grid-gap: 20px;
   padding: 20px;
 `
+
 const Categories = styled.div`
   background: lightgrey;
   color: black;
   height: 50px;
   text-align: start;
-  display: grid;
+  display: flex;
   align-items: center;
   padding: 10px;
   box-shadow: 2px 2px 5px black;
@@ -23,15 +25,41 @@ export default class Private extends Component {
   render() {
     return (
       <Wrapper>
-        <Categories>Arbeitsrecht</Categories>
-        <Categories>Erbrecht</Categories>
-        <Categories>Familienrecht</Categories>
-        <Categories>Vertragsrecht</Categories>
-        <Categories>Schuldrecht</Categories>
-        <Categories>Sachenrecht</Categories>
-        <Categories>Arbeitsrecht</Categories>
-        <Categories>Arbeitsrecht</Categories>
-        <Categories>Arbeitsrecht</Categories>
+        <Categories>
+          Arbeitsrecht <Bookmark />
+        </Categories>
+        <Categories>
+          Erbrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Familienrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Vertragsrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Schuldrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Sachenrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Arbeitsrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Arbeitsrecht
+          <Bookmark />
+        </Categories>
+        <Categories>
+          Arbeitsrecht
+          <Bookmark />
+        </Categories>
       </Wrapper>
     )
   }
