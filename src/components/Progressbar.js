225f6dff1eb1b0ac656df03d225074740a579d65
Caroline Jörgens <caroline.jorgens@aol.com>
2018-12-03 16:55:44 +0100

    No circle but progress in number

--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -2,26 +2,36 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const Progress = styled.div`
-  height: 100px;
-  width: 100px;
   background: lightgrey;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  margin: 20px;
+  margin: 5px;
+  margin-top: 20px;
+  display: -webkit-box;
+  border-radius: 10px 10px 10px;
+  border: 5px solid white;
+  color: red;
+  display: grid;
+  text-align: start;
+  font-weight: bold;
+  padding: 2px;
 
   &::after {
     content: '';
     display: block;
-    border-radius: 50%;
     width: ${props => props.percentage * 100}%;
     background: blue;
-    transition: width 0.3s ease;
+    height: 30px;
+    margin-top: 10px;
+    border-radius: inherit;
   }
 `
 class Progressbar extends Component {
   render() {
-    return <Progress percentage={this.props.percentage} />
+    return (
+      <Progress percentage={this.props.percentage}>
+        {'Dein bisheriger Lernfortschritt: '}
+        {Math.round(this.props.percentage * 100)}%
+      </Progress>
+    )
   }
 }
 
